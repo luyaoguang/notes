8e029a6e2173adbcbd752e5ae8598f52cc23b7dc
路耀广 <haishi.lyg@raycloud.com>
2019-08-06 15:45:54 +0800

    提交

--- Underscore源码/3.迭代器iteratee.js
+++ Underscore源码/3.迭代器iteratee.js
@@ -54,7 +54,49 @@ console.log(data);//[3,6,9]
                 callback.call(target, key, target[key]);
             }
         }
+    },
+    _.map = function(obj, iteratee, context) {
+        var iteratee = cb(iteratee, context),//生成不同功能的迭代器
+            keys = !_.isArray(obj) && Object.keys(obj),//分辨obj是数组还是object对象
+            length = (keys || obj).length,
+            result = Array(length),
+            index = 0;
+        for(; index < length; index++) {//循环执行迭代器
+            var currentKey = keys ? keys[index] : index;
+            result[index] = iteratee(obj[currentKey], index, obj);//当前选项 下标 对象本身
+        }
+        return result;
     }
-   _.mixin(_);
+    var cb = function(iteratee, context, count) {//接收 迭代器 上下文对象 参数成员数量
+        //如果没有传入迭代器 那么就返回默认迭代器
+        if(iteratee == null) {
+            return _.identity;
+        }
+        //如果传入的迭代器是函数 就调用optimizeCb优化迭代器
+        if(_.isFunction(iteratee)) {
+            return optimizeCb(iteratee, context, count);
+        }
+    }
+    //优化迭代器
+    var optimizeCb = function(func, context, count) {
+        if(context == void(0)) {//如果没有传入上下文对象 就直接返回传入的迭代器
+            return func;
+        }
+        switch(count == null ? 3 : count) {//默认为3
+            case 1:
+                return function(value) {
+                    return func.call(context, valvue);
+                }
+            case 3:
+                return function(value, index, obj) {
+                    return func.call(context, value, index, obj);//当前选项 下标 对象本身
+                }
+        }
+    }
+    //默认的迭代器
+    _.identity = function(value) {
+        return value;
+    }
+    _.mixin(_);
     root._ = _;
 })(this);
--- Underscore源码/5.reduce和真值检测函数.js
+++ Underscore源码/5.reduce和真值检测函数.js
@@ -1,3 +1,5 @@
+
+//reduce函数
 function createReduce (dir) {
     var reduce = function(obj,iteratee,memo,init) {
         var keys = !isArray(obj) && Object.keys(obj),
@@ -43,4 +45,40 @@ function optimizeCb(func,context,count) {
                 return func.call(context, memo, value, index, obj);
             }
     }
+}
+
+//真值检测函数
+_.filter = function(obj, predicate, context) {//被遍历对象 迭代器 上下文对象
+    var results = [];
+    predicate = cb(predicate, context);//生成迭代器
+    _.each(obj, function(value, index, list) {
+        if(predicate(value, index, list)) results.push(value);
+    })
+    return results;
+}
+_.each = function(target, callback) {
+    var key, i = 0;
+    if(_.isArray(target)) {//如果是数组
+        var length = target.length;
+        for(; i < length; i++) {
+            callback.call(target, target[i], i);
+        }
+    } else {//如果是类数组对象
+        for(key in target) {
+            callback.call(target, key, target[key]);
+        }
+    }
+}
+var cb = function(iteratee, context, count) {//接收 迭代器 上下文对象 参数成员数量
+    //如果没有传入迭代器 那么就返回默认迭代器
+    if(iteratee == null) {
+        return _.identity;
+    }
+    //如果传入的迭代器是函数 就调用optimizeCb优化迭代器
+    if(_.isFunction(iteratee)) {
+        return optimizeCb(iteratee, context, count);
+    }
+}
+_.identity = function(value) {
+    return value;
 }
